eba2ca8a87afecd2c053f910f566f1ef745d4d95
JulienS <j.seny@student.maastrichtuuniversity.nl>
2026-02-27 15:50:05 +0100

    fixed indentation

--- food.py
+++ food.py
@@ -16,7 +16,7 @@ from turtle import Turtle
 class Food(Turtle):
     def __init__(self, screen_width: int, screen_height: int, cell_size: int = 20):
         super().__init__()
- # save settings so we can reuse them later, if needed 
+# save settings so we can reuse them later, if needed 
         self.cell_size = cell_size
     
 # how the turtle food will look like 
@@ -44,38 +44,37 @@ class Food(Turtle):
 # making sure the food does not spawn on the snake 
     def refresh(self, occupied_positions):
     
-    # the different food types, and the points they give, including the sparkly strawberry which gives 5 points instead of 3 
- 
- #pick a food type
- fruit_type = random.choice(["orange", "plum", "strawberry", "sparkly"])
+    # the different food types, and the points they give, including the sparkly strawberry which gives 5 points instead of 3  
+    #pick a food type
+        fruit_type = random.choice(["orange", "plum", "strawberry", "sparkly"])
 
-    if fruit_type == "orange":
-        self.color("orange")
-        self.points = 2
-        self.shapesize(0.5, 0.5)
+        if fruit_type == "orange":
+            self.color("orange")
+            self.points = 2
+            self.shapesize(0.5, 0.5)
 
-    elif fruit_type == "plum":
-        self.color("purple")
-        self.points = 2
-        self.shapesize(0.5, 0.5)
+        elif fruit_type == "plum":
+            self.color("purple")
+            self.points = 2
+            self.shapesize(0.5, 0.5)
 
-    elif fruit_type == "strawberry":
-        self.color("red")
-        self.points = 3
-        self.shapesize(0.5, 0.5)
+        elif fruit_type == "strawberry":
+            self.color("red")
+            self.points = 3
+            self.shapesize(0.5, 0.5)
 
 #SPARKLY STRAWBERRY, YESSSS
-    elif fruit_type == "sparkly":
-        self.color("gold")
-        self.points = 5
-        self.shapesize(0.7, 0.7)
+        elif fruit_type == "sparkly":
+            self.color("gold")
+            self.points = 5
+            self.shapesize(0.7, 0.7)
     
 
     #snap to the grid, and make sure its avoids the snake body
-    while True:
-        x = random.randrange(-280, 280, 20)
-        y = random.randrange(-280, 280, 20)
+        while True:
+            x = random.randrange(-280, 280, 20)
+            y = random.randrange(-280, 280, 20)
 
-        if (x, y) not in occupied_positions:
-            self.goto(x, y)
-            break
+            if (x, y) not in occupied_positions:
+                self.goto(x, y)
+                break
